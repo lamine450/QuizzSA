5ac33cd18ecdf3338617dc5536e1f9e226746d1f
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-05-29 05:05:04 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIZZSA</title>
+    <link rel="stylesheet" type="text/css" href="./public/css/style.css">
+</head>
+<body>
+        <div class="header">
+            <div class="logo"></div>
+            <div class="header-text">J'ai le plaisir de jouer</div>
+        </div>
+        <div class="content"></div>
+    <?php
+    
+
+require_once("./vues/connexion.php");
+
+
+    
+    ?>
+    
+</body>
+</html>
