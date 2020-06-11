f7401ab0690b124f3c7cfd0859e187441d786398
lamine450 <62139787+lamine450@users.noreply.github.com>
2020-06-11 04:24:18 +0000

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -1,39 +1,74 @@
-
-<div class="connexion">
-          <h3>Connexion</h3>
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Homepage </title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style>
+    
+    </style>
+    <!-- bootstrap local -->
+    <link rel="stylesheet" href="asset/CSS/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid ">
+       <!-- header -->
+          <div class="row">
+              <div class="col-lg-6 col-md-8 col-sm-12">
+                <div class="text-center text-secondary">
+                    
+                </div>   
+              </div>   
+          </div>
+       <!-- end_header  -->
  
-<form  style ="
-    background-color: whith; margin-top:95px; height: 500%;
-            " id="connexion-form" action="Javascript:void(0);">
-        <div  class="container">
-            <div class="form-group row">
-            <div class="col-md-3"> </div>
-            <div class="col-md-7"><input  style ="background-color: cover;background-color:white ;width: 130%; height: 3em; border-radius: 1%; position:relative; top:250%; right: 25%;" type="text" name="login" placeholder="login"></div>
-            </div>  
-            <div class="form-group row">
-            <div class="col-md-3"></div>
-            <div class="col-md-7"><input style="background-color: cover;background-color:white ;width: 130%; height: 3em; border-radius: 1%; position: relative; top:300%; right: 25%;" type="password" name="pwd" placeholder="password"></div>
-            </div>  
-            <div class="form-group">
-            <div class="col-md-7"><input style="background-color: Aqua; width:7em; position:absolute;top: 10em;left: 4.5em" type="button" id="connexion" name="connexion" value="connexion" onclick="getFormData();"></div>
-           <br><br> <div class="col-md-7"><a href="index.php?lien=inscription" class="link-form">S'inscrire pour jouer </a></div>
+            <!--Modal: Contact form-->
+            
           
-           <style>
-              
-               .link-form
-               {
-                margin-left: 25px;
-                font-size: 20px;
-                font-weight: bold;
-                color: silver;
-                text-decoration: none;
-                position: absolute;
-                top: 5.5em;
-                left: 10em;
-               }
-           </style>
-            </div>  
+              <!--Content-->
+              <div class="" style="position: absolute; left:30%;" >  
+          
+               
+                <!--Body-->
+            
 
-</form> 
-</div>
+                  <!-- formulaire -->
+                  <form action="" method="post"> 
+                    <!-- Default input name -->
+                    <label for="defaultFormNameModalEx">LOGIN</label>
+                    <input type="text" id="input1" name="login" id="defaultFormNameModalEx" class="form-control form-control-sm">
+                    <span id="login_error" class="text-danger"><?php echo $login_error; ?></span>
+                    <br>
+            
+                    <!-- Default input email -->
+                    <label for="defaultFormEmailModalEx">PASSWORD</label>
+                    <input type="password" id="input2" name="password" id="defaultFormEmailModalEx" class="form-control form-control-sm">
+                    <span id="password_error" class="text-danger"><?php echo  $password_error; ?></span> 
+                    <br>
+            
+                    <div class="text-center mt-4 mb-2 text-align-between">
+                      <button type="submit" id="bouton" name="sign_in" class="btn btn-info">Connexion</button>
+                      <button type="submit" name="create_account" class="btn  ">s'inscrire pour joue</button>
+                    </div>
+
+                  </form>  
+          
+                </div> 
+              </div>
+              
+            </div>
+    
+          
+        
+       
+    </div>    
+        
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!-- bootstrap.js local -->
+    <link rel="stylesheet" href="asset/JS/bootstrap.js">
+  </body>
+</html>
